82819f551bfe97aaad9cfe73416266063a6d2045
rj <jarquinr121@gmail.com>
2025-09-17 19:23:43 -0700

    wsl,apple,linux, resolve child process

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { execFile } from "node:child_process";
 import data from "./questions.json" with { type: "json" }
+import os from "os"
 import open from 'open';
+import { resolve } from "node:path";
 
 const envs = function validateEnvs() {
   const notesPath = process.env.NOTES;
@@ -10,16 +12,48 @@ const envs = function validateEnvs() {
   return notesPath;
 };
 
-const openNotes = function openNotesHandler(notesPath) {
+const platform = function detectPlatform(notesPath) {
+  const machine = os.platform()
+  let args = []
+  let command = ""
+  switch (machine) {
+    case "win32":
+      console.log("🪟")
+      command = "cmd.exe"
+      args = ["/c", notesPath]
+      break
+    case "darwin":
+      console.log("🍎")
+      command = "open"
+      args = [notesPath]
+      break
+    case "linux":
+      if (os.release().includes("microsoft")) {
+        console.log("🐧 WSL")
+        command = "cmd.exe"
+        args = ["/c", notesPath]
+        break
+      } else {
+        console.log("🐧")
+        command = "xdg-open"
+        args = [notesPath]
+      }
+      break
+  }
+  return { command, args };
+}
+
+const openNotes = async function openNotesHandler(notesPath) {
   if (!notesPath) {
     console.log("No environment variable in .env == no notes 😥");
   } else {
-    execFile("cmd.exe", ["/c", notesPath], (error) => {
+    const { command, args } = platform(notesPath)
+    execFile(command, args, (error) => {
       if (error) throw error;
       console.log("✅ Notes opened!");
+      resolve()
     });
   }
-  return
 };
 
 const patternArray = function pickRandomPatternArray() {
@@ -30,17 +64,15 @@ const patternArray = function pickRandomPatternArray() {
 };
 
 const question = async function pickRandomLeetcodeQuestion(array) {
-    const index = Math.floor(Math.random() * array.length) 
-    await open(array[index])
-    return
+  const index = Math.floor(Math.random() * array.length)
+  await open(array[index])
 }
 
 const start = async function getShitStarted() {
   const notesPath = envs();
-  openNotes(notesPath);
+  await openNotes(notesPath);
   const array = patternArray()
   await question(array)
-  return
 };
 
 start();
